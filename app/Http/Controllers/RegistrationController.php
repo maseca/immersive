1767cd44b1f4435f1a15bffc565a2660b77d6b7d
luisjg <luis.guzman@metalab.csun.edu>
2017-06-12 19:51:00 -0700

    Add the registration controller which handles the form request and route parameters.

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegistrationFormRequest;
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    /**
+     * Retrieves the registration form
+     * @return view the front end view
+     */
+    public function getRegistration(Request $request)
+    {
+    	if($request->has('name'))
+    		$name = $request['name'];
+    	return view('pages.login.login', compact('name'));
+    }
+
+    /**
+     * Handles the form input
+     * @param  RegistrationFormRequest $request the post request from the form
+     * @return JSON the complete request
+     */
+    public function postRegistration(RegistrationFormRequest $request)
+    {
+    	return $request->all();
+    }
+
+    /**
+     * Handles the route parameter passed in from the front end
+     * @param  String $id the given id
+     * @return String the given id
+     */
+    public function handleRouteParameter($id)
+    {
+    	return $id;
+    }
+}
